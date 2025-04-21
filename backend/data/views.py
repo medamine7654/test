cb7f95ea77d808068ebbce683208aa095876df0b
medamine <medaminea61@gmail.com>
2025-04-21 01:24:36 +0100

    added in views

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
-from rest_framework import viewsets, perrmissions
+from rest_framework import viewsets, perrmissions 
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
 
 # Create your views here.
+
+class SupermarketSalesViewset(viewset.Viewset):
+    permession_classes = [permessions.AllowAny]
+    queryset = SupermarketSales.objects.all()
+    serializer_class = SupermarketSalesSerializers
+
+
+    def list(self , request):
+        queryset = SupermarketSales.object.all()
+        serializer = self.serliazer_class(queryset , many=true)
+        return  Response(serializer.data)
+
